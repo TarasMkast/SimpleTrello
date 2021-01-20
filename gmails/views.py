36534655cd1ab6b511988ae589e2e7d3b8439f57
Taras YLT <taras.m@yleaf.co>
2021-01-20 18:12:41 +0200

    test view

--- gmails/views.py
+++ gmails/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render, HttpResponse
-from django.core.mail import send_mail as sm
+from django.shortcuts import render, HttpResponse, redirect
+from gmails.manager import ManagerAuthMail
+from gmails.forms import PasswordForm, MailForm
 
 
-def send_email(request):
-    res = sm(
-        subject='Підтвердження пошти',
-        message='Код підтвердження пошти: ',
-        from_email='',
-        recipient_list=['taras.m@yleaf.co'],
-        fail_silently=False
-    )
-    #return HttpResponse('Email sent: ' + str(res))
+def send_email_view(request):
+    if request.method == "POST":
+        form = MailForm(request.POST)
+        print(form)
+        if form.is_valid():
+            manage = ManagerAuthMail(form)
+            manage.make_random_password()
+            manage.send_email()
+        return redirect('verify')
+    form = MailForm()
+    return render(request, 'authMail/authMail.html', {'form_mail': form})
+
+
+def confirmation_view(request):
+
+    return HttpResponse('Good')
