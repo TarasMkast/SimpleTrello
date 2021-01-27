15062070a9539bacbb93f89e1a4d5492eadcaa86
Taras YLT <taras.m@yleaf.co>
2021-01-27 11:53:27 +0200

    merge dev

--- gmails/views.py
+++ gmails/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render, HttpResponse
-from django.core.mail import send_mail as sm
+from datetime import timedelta
+from django.shortcuts import (render,
+                              HttpResponse,
+                              redirect,
+                              )
+from django.utils import timezone
+
+from gmails.manager import ManagerAuthMail
+from gmails.forms import (PasswordForm,
+                          MailForm,
+                          )
+from gmails.models import MailPassword
 
 
-def send_email(request):
-    res = sm(
-        subject='Підтвердження пошти',
-        message='Код підтвердження пошти: ',
-        from_email='',
-        recipient_list=['taras.m@yleaf.co'],
-        fail_silently=False
-    )
-    #return HttpResponse('Email sent: ' + str(res))
+def send_email_view(request):
+    if request.method == "POST":
+        form = MailForm(request.POST)
+        if form.is_valid():
+            message = form.cleaned_data.get('mail_form')
+            manage = ManagerAuthMail(message)
+            send_password = manage.make_random_password()
+            manage.send_email(send_password)
+            db_send_password_mail = MailPassword(send_mail=message, send_password=send_password)
+            db_send_password_mail.save()
+        return redirect('verify')
+    form = MailForm()
+    return render(request, 'authMail/authMail.html', {'form_mail': form})
+
+
+def confirmation_view(request):
+    db_send_password_mail = MailPassword.objects.values_list('send_password')
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            input_password = form.cleaned_data.get('form_password')
+            for db in db_send_password_mail:
+                if input_password == db[0]:
+                    db_send_password_m = MailPassword.objects.get(send_password=db[0])
+                    now = timezone.now() - timedelta(minutes=60)
+                    if db_send_password_m.date > now:
+                        db_send_password_m.input_password = db[0]
+                        db_send_password_m.is_verify = True
+                        db_send_password_m.save()
+                        return HttpResponse('Вітаємо, ви підтвердили вашу пошту!')
+                    return HttpResponse('Час активація вийшов! Пошта не активована, спробуйте знову.')
+            return HttpResponse('Код не вірний!')
+    form = PasswordForm()
+    return render(request, 'authMail/verifyMail.html', {'form_verify': form})
